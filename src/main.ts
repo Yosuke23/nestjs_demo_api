762c2fa08650291a97eedc33ca52cacb72e7927f
y-narumi <yosuke4123@gmail.com>
2022-08-10 14:42:27 +0900

    swagger導入

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger
+  const options = new DocumentBuilder().setTitle('nestjs_demo_api').build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
